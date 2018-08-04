a6b82771a16d23cb0fb8104a6676f75e5ee2f992
kmbruce1986 <kmbruce1986@gmail.com>
2018-08-04 08:59:04 +0100

    adds delete one method to tickets

--- models/ticket.rb
+++ models/ticket.rb
@@ -32,4 +32,10 @@ class Ticket
     SqlRunner.run(sql, values)
   end
 
+  def delete
+    sql = "DELETE FROM tickets WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
 end
