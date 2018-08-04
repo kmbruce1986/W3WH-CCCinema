e3cdcfb9900cebcf6aa70ab2d113e0823cab7c0d
kmbruce1986 <kmbruce1986@gmail.com>
2018-08-04 08:57:12 +0100

    adds delete one function to customer

--- models/customer.rb
+++ models/customer.rb
@@ -32,6 +32,12 @@ class Customer
     SqlRunner.run(sql, values)
   end
 
+  def delete
+    sql = "DELETE FROM customers WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
 
 
 end
