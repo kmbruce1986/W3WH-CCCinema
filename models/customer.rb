6baaa83e592167e1552433c88213b797af9148dd
kmbruce1986 <kmbruce1986@gmail.com>
2018-08-04 08:43:57 +0100

    adds update method to customer

--- models/customer.rb
+++ models/customer.rb
@@ -26,6 +26,12 @@ class Customer
     return result
   end
 
+  def update
+    sql = "UPDATE customers SET (name, funds) = ($1, $2) WHERE id = $3"
+    values = [@name, @funds, @id]
+    SqlRunner.run(sql, values)
+  end
+
 
 
 end
