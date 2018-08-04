ac12ed04f3bcfa0b36caffef670d9943709f767c
kmbruce1986 <kmbruce1986@gmail.com>
2018-08-04 09:27:50 +0100

    adds method to customer to show reduced funds when tickets bought

--- models/customer.rb
+++ models/customer.rb
@@ -50,5 +50,11 @@ class Customer
     return films.map {|film| Film.new(film)}
   end
 
+  def buy_ticket
+    sql = "SELECT SUM(films.price) FROM films INNER JOIN tickets ON films.id = tickets.film_id WHERE tickets.customer_id = $1"
+    values = [@id]
+    film_price = SqlRunner.run(sql, values).first
+    return @funds - film_price['sum'].to_i
+  end
 
 end
